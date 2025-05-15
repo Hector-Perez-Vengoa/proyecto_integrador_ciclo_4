84a8e7337dbd3d8e6171ddd4dc7f7c7057991486
AlessandroADP <tu_correo_electronico@example.com>
2025-05-15 01:18:59 -0500

    dando orden

--- backend/back_django_srvt/config/settings.py
+++ backend/back_django_srvt/config/settings.py
@@ -17,7 +17,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -29,15 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-
-
-
-
-
-
 SITE_ID = 2
-
 
 # Application definition
 
@@ -108,10 +99,6 @@ CORS_ALLOWED_ORIGINS = [
  ]
  
 CORS_ALLOW_CREDENTIALS = True
-
-
-
-
 
 
 MIDDLEWARE = [
