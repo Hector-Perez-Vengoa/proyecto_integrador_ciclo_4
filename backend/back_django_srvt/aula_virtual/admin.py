99523f19ef05d4a959a1d78139195bcb6431819a
git config --global AlessandroADP <alessandro.davila@tecsup.edu.pe>
2025-05-20 16:17:49 -0500

    ADMIN EN DJANGO

--- backend/back_django_srvt/aula_virtual/admin.py
+++ backend/back_django_srvt/aula_virtual/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import DepartamentoDB, CarreraDB, CursoDB, ProfesorDB,AulaVirtualDB
 
 # Register your models here.
+@admin.register(DepartamentoDB)
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_fields = '__all__'
+    search_fields = ('nombre', 'codigo')
+    list_filter = ('jefe',)
+
+
+@admin.register(CarreraDB)
+class CarreraAdmin(admin.ModelAdmin):
+    list_fields = '__all__'
+    search_fields = ('nombre', 'codigo')
+    list_filter = ('departamento',)
+
+
+@admin.register(CursoDB)
+class CursoAdmin(admin.ModelAdmin):
+    list_fields = '__all__'
+    search_fields = ('nombre',)
+    list_filter = ('carrera',)
+
+
+@admin.register(ProfesorDB)
+class ProfesorAdmin(admin.ModelAdmin):
+    list_fields = '__all__'
+    search_fields = ('nombre', 'apellidos', 'codigo')
+
+
+@admin.register(AulaVirtualDB)
+class AulaVirtualAdmin(admin.ModelAdmin):
+    list_fields = '__all__'
+    search_fields = ('nombre', 'codigo')
+    list_filter = ('profesor',)
+
+
+
